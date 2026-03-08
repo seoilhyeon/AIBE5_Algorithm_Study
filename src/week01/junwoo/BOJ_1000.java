17b162608cacb074f29b5a0842daff6d9f785208
yakjun01 <dirwnsdl0106@gmail.com>
2026-03-08 14:19:30 +0900

    Initial commit for week 1

---
+++ src/week01/junwoo/BOJ_1000.java
@@ -0,0 +1,17 @@
+package week01.junwoo;
+
+import java.util.Scanner;
+
+public class BOJ_1000 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int result = a + b;
+
+        System.out.println(result);
+
+        sc.close();
+    }
+}
