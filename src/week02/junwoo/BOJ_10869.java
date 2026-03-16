70cc3095972a75cdc18a67f0b6f42e4019fa5252
yakjun01 <dirwnsdl0106@gmail.com>
2026-03-16 10:38:15 +0900

    [BOJ] 10869, 10998 - JAVA

---
+++ src/week02/junwoo/BOJ_10869.java
@@ -0,0 +1,18 @@
+package week02.junwoo;
+
+import java.util.Scanner;
+
+public class BOJ_10869 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+
+        double result = a / b;
+
+        System.out.println(result);
+
+        sc.close();
+    }
+}
---
+++ src/week02/junwoo/BOJ_10998.java
@@ -0,0 +1,18 @@
+package week02.junwoo;
+
+import java.util.Scanner;
+
+public class BOJ_10998 {
+    public static void main(String[] args) {
+    
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int result = a * b;
+
+        System.out.println(result);
+
+        sc.close();
+    }
+}
