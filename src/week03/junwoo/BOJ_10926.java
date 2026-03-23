e4c3c339aa04ec33110199c0e951b5a2f5f9782e
yakjun01 <dirwnsdl0106@gmail.com>
2026-03-23 09:25:27 +0900

    [BOJ] 10869, 10926 - JAVA

---
+++ src/week03/junwoo/BOJ_10926.java
@@ -0,0 +1,15 @@
+package week03.junwoo;
+
+import java.util.Scanner;
+
+public class BOJ_10926 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String id = sc.next();
+
+        System.out.println(id + "??!");
+
+        sc.close();
+    }
+}
