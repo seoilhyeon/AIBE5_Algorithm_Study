38c29a84fb4590a2a34e786105422bd48c465be1
slyhyun <lipsul061722@jbnu.ac.kr>
2026-03-31 22:26:34 +0900

    [백준 1259] 팰린드롬수

---
+++ week05/anhyeon/Solution_BOJ_1259.java
@@ -0,0 +1,26 @@
+package week05.anhyeon;
+
+import java.util.Scanner;
+
+public class Solution_BOJ_1259 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String s = sc.nextLine();
+
+            if (s.equals("0")) {
+                break;
+            }
+
+            StringBuffer sb = new StringBuffer(s);
+            String rs = sb.reverse().toString();
+
+            if (s.equals(rs)) {
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+            }
+        }
+    }
+}
