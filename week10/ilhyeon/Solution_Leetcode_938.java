df9304ff768476ac5a6ad61dadab9746421f6ebc
seoilhyeon <ilhyeon0326@gmail.com>
2026-05-06 09:32:36 +0900

    [리트코드 938] Range Sum of BST

---
+++ week10/ilhyeon/Solution_Leetcode_938.java
@@ -0,0 +1,24 @@
+package week10.ilhyeon;
+
+import week09.ilhyeon.TreeNode;
+
+public class Solution_Leetcode_938 {
+
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.val < low) {
+            return rangeSumBST(root.right, low, high);
+        }
+
+        if (root.val > high) {
+            return rangeSumBST(root.left, low, high);
+        }
+
+        return root.val
+                + rangeSumBST(root.left, low, high)
+                + rangeSumBST(root.right, low, high);
+    }
+}
